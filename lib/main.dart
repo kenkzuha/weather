b4fdcfab277f78d45c478b2f7a1a92c9873842e0
kenkzuha <kenkzuha@gmail.com>
2024-10-31 00:41:22 +0900

    Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.cyan, Colors.black],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Text("Hello World"),
+        ),
+      ),
+    ),
+  );
 }
