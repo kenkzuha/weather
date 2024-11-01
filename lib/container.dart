6cd48a8374055496beaac65209e085c045396a07
kenkzuha <kenkzuha@gmail.com>
2024-11-02 01:25:59 +0900

    Added Button

--- lib/container.dart
+++ lib/container.dart
@@ -1,39 +1,1 @@
-import 'package:flutter/material.dart';
-
-class GradientContainer extends StatelessWidget {
-  final Color color1;
-  final Color color2;
-
-  const GradientContainer(this.color1, this.color2, {super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color1,color2],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-      child: Container(
-        child: Align(
-          alignment: Alignment.center,
-          child: Image.asset('assets/images/one.png', width: 200),
-        )
-      )
-    );
-  }
-}
-
-class StyledText extends StatelessWidget {
-  final String text;
-  const StyledText(this.text, {super.key});
-
-  @override
-  Widget build(context) {
-    return Text(text,
-        style: const TextStyle(color: Colors.black, fontSize: 28));
-  }
-}
-
+import 'package:flutter/material.dart';class GradientContainer extends StatelessWidget {  final Color color1;  final Color color2;  void spin(){  }  const GradientContainer(this.color1, this.color2, {super.key});  @override  Widget build(context) {    return Container(      decoration: BoxDecoration(        gradient: LinearGradient(          colors: [color1,color2],          begin: Alignment.topLeft,          end: Alignment.bottomCenter,        ),      ),      child: Center(        child: Column(          children: [            Image.asset(                'assets/images/one.png',                width: 200            ),            TextButton(              onPressed: spin,              child: const Text('Roll Dice'),            )          ],        )      ),    );  }}class StyledText extends StatelessWidget {  final String text;  const StyledText(this.text, {super.key});  @override  Widget build(context) {    return Text(text,        style: const TextStyle(color: Colors.black, fontSize: 28));  }}
